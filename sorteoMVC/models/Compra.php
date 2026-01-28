6c9ef2d3d3a1b29f4407d6ed61ff30cc42cd22b3
Natalia T-94 <nattecheira@gmail.com>
2026-01-28 12:59:33 -0300

    sorteoMVC-Models-Compra

--- sorteoMVC/models/Compra.php
+++ sorteoMVC/models/Compra.php
@@ -3,21 +3,21 @@ namespace Model;
 
 class Compra extends ActiveRecord{
     //Base de Datos
-    protected static $tabla = 'compra';//la informacion la va a sacer de la tabla de usuarios
-    protected static $columnasDB = ['compraId', 'productoId', 'numero', 'precioTotal', 'usuarioId'];
+    protected static $tabla = 'compra';
+    protected static $columnasDB = ['compraId', 'productoId', 'usuarioId', 'numero', 'precioTotal'];
     
     public $compraId;
     public $productoId;
+    public $usuarioId;
     public $numero;
     public $precioTotal;
-    public $usuarioId;
 
     public function __construct($args = []){
         $this->compraId = $args['compraId'] ?? null;
         $this->productoId = $args['productoId'] ?? '';
+        $this->usuarioId = $args['usuarioId'] ?? '';
         $this->numero = $args['numero'] ?? '';
         $this->precioTotal = $args['precioTotal'] ?? '';
-        $this->usuarioId = $args['usuarioId'] ?? '';
         
     }
 }
