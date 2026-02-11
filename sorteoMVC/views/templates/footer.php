8fbc6c8503b91d455328723518c24387478d29c8
Natalia T-94 <nattecheira@gmail.com>
2026-02-11 18:15:14 -0300

    sorteoMVC -scss header

--- sorteoMVC/views/templates/footer.php
+++ sorteoMVC/views/templates/footer.php
@@ -1,21 +1,31 @@
-    <footer class="contenedor contenido-footer">
-        <div class="registro">
-            <form class="formulario" action="">
-                <div class="formulario-datos">
+<footer class="contenedor contenido-footer">
+    <div class="registro">
+
+        <form class="formulario" action="">
+            <div class="formulario-datos">
+                <div class="campo">
                     <label for="nombre">Nombre</label>
-                    <input type="text" class="form-control" placeholder="Nombre" name="nombre" id="nombre">
+                    <input type="text" placeholder="Nombre" id="nombre">
+                </div>
+
+                <div class="campo">
                     <label for="telefono">Telefono</label>
-                    <input type="tel" class="form-control" placeholder="Telefono" name="telefono" id="telefono">
-            </form>
+                    <input type="tel" placeholder="Telefono" id="telefono">
+                </div>
             </div>
-            <div class="lista">
-                <h4 class="lista-titulo">Tus Numeros</h4>
-                <ul class="lista-num"></ul>
-            </div>
-            <div class="precio">
-                <label for="total">TOTAL</label>
-                <input type="num" class="form-control" placeholder="total" name="total" id="total">
-                <input type="submit" class="comprar" value="Comprar">
-            </div>
+
+        </form>
+
+        <div class="lista">
+            <h4 class="lista-titulo">Tus Numeros</h4>
+            <ul class="lista-num"></ul>
         </div>
-    </footer>
+
+        <div class="precio">
+            <label for="total">TOTAL</label>
+            <input type="number" class="form-control" placeholder="total" name="total" id="total">
+            <input type="submit" class="comprar" value="Comprar">
+        </div>
+
+    </div>
+</footer>
--- sorteoMVC/views/templates/header.php
+++ sorteoMVC/views/templates/header.php
@@ -1,13 +1,16 @@
     <header class="header">
         <div class="contenedor contenido-header">
-            <div class="logo">
-                <img src="/build/img/lottery.png" alt="icono">
-                <h1 class="titulo">Sorteo Inicio de Ciclo Escolar!!!</h1>
+            <div class="encabezado">
+                <img class="encabezado-logo" width="70" height="70" src="https://img.icons8.com/external-others-pike-picture/50/external-Card-Lotto-lotto-others-pike-picture.png" alt="external-Card-Lotto-lotto-others-pike-picture"/>
+                <h1 class="encabezado-titulo">Sorteo Inicio de Ciclo Escolar!!!</h1>
             </div>
             <div class="producto">
-                <img src="build/img/ventilador2.webp" alt="Foto Producto">
-                <h2 class="producto-nombre"><?= $producto->nombre ?></h2>
-                <p class="producto-descripcion"><?= $producto->descripcion ?></p>
+                <div class="producto-imagen"></div>
+                <!-- <img src="public/build/img/ventilador5.jpg" alt="Foto Producto"> -->
+                 <div class="producto-info">
+                     <h2 class="producto-nombre"><?= $producto->nombre ?></h2>
+                     <p class="producto-descripcion"><?= $producto->descripcion ?></p>
+                 </div>
             </div>
         </div>
     </header>
